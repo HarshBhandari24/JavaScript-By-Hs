a24922bff2953d49f8dab30a603787ddfd2dfd02
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-08-19 07:46:41 +0000

    continue

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+// console.log("Js")
+const accountId = 12345
+let accountEmail = "hb@gmail.com"
+var accountPassword = "56789"
+accountCity = "Jaipur"
+let acocountState;
+// accountId=2 //not allowed
+/* var is not used in todays time
+bacause of issue in block scope and functional scope */
+accountEmail = "hbrb@gmail.com"
+accountPassword = "595956"
+accountCity = "Pune"
+console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, acocountState])
