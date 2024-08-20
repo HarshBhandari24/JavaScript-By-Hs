ea0728c6623229a53c6d4992e40d88eadafd918f
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-08-20 15:42:39 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,16 @@
+console.log("Javascript")
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using node js not browser
+//null =>standalone value
+
+let name ="Harsh"
+let age =24
+let isLoggedIn =false
+
+console.log(typeof "harsh");
+console.log(typeof age);
+console.log(typeof 18);
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+console.log(name);
