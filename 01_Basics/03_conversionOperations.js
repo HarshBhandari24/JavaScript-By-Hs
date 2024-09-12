28ffb0776efac12fe5ec0af18e92511dab7a3ba7
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-09-12 07:43:12 +0000

    commit

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -29,3 +29,30 @@ let someNumber = 33
 let stringNumber =String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// Operations
+let digit =4;
+let negdigit =-digit;
+/*console.log(negdigit);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+console.log(3%2);*/
+
+let str1 = "Hello"
+let str2 = " Harsh"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+4) * (5/4) *(5-2)); //uses more small parenthesis to make clean code.
+console.log(true);
+console.log(true+);
+console.log(+true);
+
