ebc94c205ca31c749a8b712d7cfd95c1c280f098
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-09-21 11:11:35 +0000

    NUm

---
+++ 01_Basics/05_numbermath.js
@@ -0,0 +1,21 @@
+const score =400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString() .length);
+console.log(balance .toFixed(2));
+const otherno =124.446
+console.log(otherno .toPrecision(5));
+const hundreds = 100000000
+console.log(hundreds .toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(25));
+console.log(Math.max(4,8,9,2));
+console.log(Math.min(5,4,3,8));
+
