10e2cb9acc7fbee6deef6b8872dd335dc1114069
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-09-24 11:25:16 +0000

    array1

---
+++ 02_Mid/01_array.js
@@ -0,0 +1,45 @@
+console.log("JAI SHREE RAM")
+
+// const myarr = [1,5,8,9,4 ,"Hero","Villan",true]  //in array we can write no. ,strings, boolean without any problem.
+// console.log(myarr[6]);
+// console.log(myarr[7]);
+
+//const newarr = new Array(1,2,5,4,6)
+// console.log(newarr);
+// console.log(newarr[1]);
+
+
+const myarr2 = [1,2,4,5,6,8]
+// console.log(myarr2[4]);
+
+// myarr2 .push(3)
+// console.log(myarr2);
+
+// myarr2 .pop()
+// console.log(myarr2);
+
+// myarr2 .pop( )
+// console.log(myarr2);
+
+// myarr2 .unshift(9)      //unshift() operation add the given digit on first position in array what we pass on it;
+// console.log(myarr2);
+
+// myarr2 .shift()         //shift() operation pop the first digit in array;
+// console.log(myarr2);
+
+// console.log(myarr2 .includes(5));
+// console.log(myarr2 .indexOf(4));
+
+// const newwarr = myarr2.join()
+// console.log(myarr2);
+// console.log(newwarr);
+// console.log(typeof(newwarr)); //when we use the join operation method it will change the the type of array to string;
+
+console.log( "A", myarr2);
+const myn1 = myarr2.slice(1,3)
+console.log(myn1);
+
+console.log("B",myarr2);
+const myn2 = myarr2.splice(1,3)
+console.log("C",myarr2);
+console.log(myn2);
