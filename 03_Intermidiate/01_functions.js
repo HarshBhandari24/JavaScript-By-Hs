3e06a6bb17a65e3fecceea250ba8f488103b0615
HarshBhandari24 <99120405+HarshBhandari24@users.noreply.github.com>
2024-09-30 08:49:12 +0000

    2_function

--- 03_Intermidiate/01_functions.js
+++ 03_Intermidiate/01_functions.js
@@ -10,10 +10,10 @@ Myname()    //Myname is reference of function () and this is execution of functi
 function Addtwono(number1,number2){  //(number1,number2) this values called Paarameters
     console.log(number1 + number2);
 }
-Addtwono(4,4)   //(4,4) this values called Arguments or values.
-Addtwono(4,"4") //44
-Addtwono(4,"a") //4a
-Addtwono(4,null) //4
+// Addtwono(4,4)   //(4,4) this values called Arguments or values.
+// Addtwono(4,"4") //44
+// Addtwono(4,"a") //4a
+// Addtwono(4,null) //4
 
 // function Addingno (Digit1,Digit2){
 //     console.log( Digit1 +Digit2);
@@ -28,7 +28,7 @@ Addtwono(4,null) //4
 
 function Addingnumber (Digit1,Digit2){
     let Answer = Digit1 + Digit2
-    return Answer   //return Digit1 + Digit2  {Alternate Way}
+    return Answer   //return Digit1 + Digit2  {Alternate Way} here we save time an dmemory to create one more object
 }
 const Answer = Addingnumber(4,2) 
 // console.log("Final Result:",Answer);
@@ -45,3 +45,37 @@ function loginusername (username = "HB")  //here we directly assign a values to 
 }
 
 console.log(loginusername("Harsh"));
+
+function calculatecartprice(val1, val2, ...num1)    //here if we use only num1 then it shows 200 the first digit values we pass to it, if we use [... spread operator] then it gives us complete arrary of passing values, (val1, val2, ...num1) if we use these the irs two digit wil store in the element the rest of values we see in the form of array.
+{
+    return num1
+}
+console.log(calculatecartprice(200,300,400,500));
+
+const user = {  
+    username : "Harsh",
+    price :400 
+}
+//here, we pass object in function.
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and the Price is ${anyobject.price}`);
+}
+handleobject(user)
+
+//Alternate way to call the object using function. in this way we do not need to pass the function reference and Exxecution in last. 
+useobject({
+    username : "Rishabh",
+    Price : 200
+})
+function useobject(newobject){
+    console.log(`Username is ${newobject.username} and Price is ${newobject.Price}`);
+}
+
+const mynewArray = [200, 1100, 300, 400]
+function returnsecondvalue (getarray){
+    return getarray[2]
+}
+// console.log(returnsecondvalue(mynewArray));
+//we also do this 
+console.log(returnsecondvalue([200, 300, 400, 500]));
+
